9cb53911c517837ae3c81c55a5e78dc93d0452d0
shrivel <shrivelk.23eie@kongu.edu>
2025-06-23 09:01:21 +0530

    shri

--- track/src/App.jsx
+++ track/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let userlist = [
+    {name: 'Alice', age: 25},
+    {name: 'Bob', age: 30},
+    {name: 'Charlie', age: 35},
+    {name: 'Shri', age: 40}
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ul>
+      {
+        userlist.map((user,index) => {
+          return (
+            <li key={index}>
+                {user.name} is {user.age} year old.
+            </li>)
+
+        })
+      }
+      </ul>
   )
 }
 
